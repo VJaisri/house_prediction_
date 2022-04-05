a2550ed1a1980882d09c02dc8d1e69dd8987253e
Jaisri V <82967826+VJaisri@users.noreply.github.com>
2022-04-05 18:31:02 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st 
+
+mod = open('bangalore_home_prices_model.pickle', 'rb')
+model = pickle.load(mod)
+
+def welcome():
+    return "Welcome"
+
+l = ("total_sqft", "bath", "bhk", " devarachikkanahalli", "1st block jayanagar", "1st phase jp nagar", "2nd phase judicial layout", "2nd stage nagarbhavi", "5th block hbr layout", "5th phase jp nagar", "6th phase jp nagar", "7th phase jp nagar", "8th phase jp nagar", "9th phase jp nagar", "aecs layout", "abbigere", "akshaya nagar", "ambalipura", "ambedkar nagar", "amruthahalli", "anandapura", "ananth nagar", "anekal", "anjanapura", "ardendale", "arekere", "attibele", "beml layout", "btm 2nd stage", "btm layout", "babusapalaya", "badavala nagar", "balagere", "banashankari", "banashankari stage ii", "banashankari stage iii", "banashankari stage v", "banashankari stage vi", "banaswadi", "banjara layout", "bannerghatta", "bannerghatta road", "basavangudi", "basaveshwara nagar", "battarahalli", "begur", "begur road", "bellandur", "benson town", "bharathi nagar", "bhoganhalli", "billekahalli", "binny pete", "bisuvanahalli", "bommanahalli", "bommasandra", "bommasandra industrial area", "bommenahalli", "brookefield", "budigere", "cv raman nagar", "chamrajpet", "chandapura", "channasandra", "chikka tirupathi", "chikkabanavar", "chikkalasandra", "choodasandra", "cooke town", "cox town", "cunningham road", "dasanapura", "dasarahalli", "devanahalli", "dodda nekkundi", "doddaballapur", "doddakallasandra", "doddathoguru", "domlur", "dommasandra", "epip zone", "electronic city", "electronic city phase ii", "electronics city phase 1", "frazer town", "gm palaya", "garudachar palya", "giri nagar", "gollarapalya hosahalli", "gottigere", "green glen layout", "gubbalala", "gunjur", "hal 2nd stage", "hbr layout", "hrbr layout", "hsr layout", "haralur road", "harlur", "hebbal", "hebbal kempapura", "hegde nagar", "hennur", "hennur road", "hoodi", "horamavu agara", "horamavu banaswadi", "hormavu", "hosa road", "hosakerehalli", "hoskote", "hosur road", "hulimavu", "isro layout", "itpl", "iblur village", "indira nagar", "jp nagar", "jakkur", "jalahalli", "jalahalli east", "jigani", "judicial layout", "kr puram", "kadubeesanahalli", "kadugodi", "kaggadasapura", "kaggalipura", "kaikondrahalli", "kalena agrahara", "kalyan nagar", "kambipura", "kammanahalli", "kammasandra", "kanakapura", "kanakpura road", "kannamangala", "karuna nagar", "kasavanhalli", "kasturi nagar", "kathriguppe", "kaval byrasandra", "kenchenahalli", "kengeri", "kengeri satellite town", "kereguddadahalli", "kodichikkanahalli", "kodigehaali", "kodigehalli", "kodihalli", "kogilu", "konanakunte", "koramangala", "kothannur", "kothanur", "kudlu", "kudlu gate", "kumaraswami layout", "kundalahalli", "lb shastri nagar", "laggere", "lakshminarayana pura", "lingadheeranahalli", "magadi road", "mahadevpura", "mahalakshmi layout", "mallasandra", "malleshpalya", "malleshwaram", "marathahalli", "margondanahalli", "marsur", "mico layout", "munnekollal", "murugeshpalya", "mysore road", "ngr layout", "nri layout", "nagarbhavi", "nagasandra", "nagavara", "nagavarapalya", "narayanapura", "neeladri nagar", "nehru nagar", "ombr layout", "old airport road", "old madras road", "padmanabhanagar", "pai layout", "panathur", "parappana agrahara", "pattandur agrahara", "poorna pragna layout", "prithvi layout", "r.t. nagar", "rachenahalli", "raja rajeshwari nagar", "rajaji nagar", "rajiv nagar", "ramagondanahalli", "ramamurthy nagar", "rayasandra", "sahakara nagar", "sanjay nagar", "sarakki nagar", "sarjapur", "sarjapur  road", "sarjapura - attibele road", "sector 2 hsr layout", "sector 7 hsr layout", "seegehalli", "shampura", "shivaji nagar", "singasandra", "somasundara palya", "sompura", "sonnenahalli", "subramanyapura", "sultan palaya", "tc palaya", "talaghattapura", "thanisandra", "thigalarapalya", "thubarahalli", "thyagaraja nagar", "tindlu", "tumkur road", "ulsoor", "uttarahalli", "varthur", "varthur road", "vasanthapura", "vidyaranyapura", "vijayanagar", "vishveshwarya layout", "vishwapriya layout", "vittasandra", "whitefield", "yelachenahalli", "yelahanka", "yelahanka new town", "yelenahalli", "yeshwanthpur")
+
+def price_pred(tot_sqft, bed, bath, loc):
+    loc_index = l.index(loc)
+    x = np.zeros(len(l))
+    x[0] = tot_sqft
+    x[1] = bath
+    x[2] = bed
+    if loc_index>=0:
+        x[loc_index] = 1
+    
+    print(loc_index)
+    print(x)
+    prediction = model.predict([x])[0]
+    print(prediction)
+    return prediction
+
+def main():
+    st.title("Bangalore House Price Prediction")
+    html_temp = """
+    <div style="background-color:rgb(68, 65, 65);padding:10px">
+    <h2 style="color:rgb(241, 153, 20);text-align:center;">WebApp for House Price Prediction</h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+
+    tot_sqft = st.text_input('Total SQFT  :',1000)
+    tot_sqft = int(tot_sqft)
+
+    bed = st.slider('Enter number of bedrooms :',min_value=1, max_value=5, value=1, step=1)
+    bed = int(bed)
+
+    bath = st.slider('Enter number of bathrooms :',min_value=1, max_value=5, value=1, step=1)
+    bath = int(bath)
+    
+    loc = st.selectbox('Select location : ', l[3::])
+    result=""
+    if st.button("Estimate Price"):
+        result=price_pred(tot_sqft, bed, bath, loc)
+        #print(result)
+        result = round(result,2)
+        st.success('The Estimated price of the house is {} Lakhs'.format(result))
+    
+    
+    if st.button("About"):
+        st.text("This is House Price Prediction Model for Bangalore City made using StreamLit")
+        st.text("Model by JAISRI V")
+
+if __name__=='__main__':
+    main()
